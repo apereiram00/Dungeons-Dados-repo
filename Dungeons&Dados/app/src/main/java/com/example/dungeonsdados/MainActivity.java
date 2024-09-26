11787f801285f0f14bbd73f482d9a70e93744065
Alvaro <alvaroppm01@gmail.com>
2024-09-26 18:36:43 +0200

    Cambios en MainActivity.java | Juego terminado

--- Dungeons&Dados/app/src/main/java/com/example/dungeonsdados/MainActivity.java
+++ Dungeons&Dados/app/src/main/java/com/example/dungeonsdados/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -23,11 +24,46 @@ public class MainActivity extends AppCompatActivity {
     int cuentaD100 = 0;
     int totalResultado = 0;
     int random;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("cuentaD4", cuentaD4);
+        savedInstanceState.putInt("cuentaD6", cuentaD6);
+        savedInstanceState.putInt("cuentaD8", cuentaD8);
+        savedInstanceState.putInt("cuentaD10", cuentaD10);
+        savedInstanceState.putInt("cuentaD12", cuentaD12);
+        savedInstanceState.putInt("cuentaD20", cuentaD20);
+        savedInstanceState.putInt("cuentaD100", cuentaD100);
+        savedInstanceState.putInt("totalResultado", totalResultado);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
         setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+        if (savedInstanceState != null) {
+            cuentaD4 = savedInstanceState.getInt("cuentaD4");
+            cuentaD6 = savedInstanceState.getInt("cuentaD6");
+            cuentaD8 = savedInstanceState.getInt("cuentaD8");
+            cuentaD10 = savedInstanceState.getInt("cuentaD10");
+            cuentaD12 = savedInstanceState.getInt("cuentaD12");
+            cuentaD20 = savedInstanceState.getInt("cuentaD20");
+            cuentaD100 = savedInstanceState.getInt("cuentaD100");
+            totalResultado = savedInstanceState.getInt("totalResultado");
+
+            // Actualizar las vistas con los valores restaurados
+            binding.idTiradasd4.setText(String.valueOf(cuentaD4));
+            binding.idTiradasd6.setText(String.valueOf(cuentaD6));
+            binding.idTiradasd8.setText(String.valueOf(cuentaD8));
+            binding.idTiradasd10.setText(String.valueOf(cuentaD10));
+            binding.idTiradasd12.setText(String.valueOf(cuentaD12));
+            binding.idTiradasd20.setText(String.valueOf(cuentaD20));
+            binding.idTiradasd100.setText(String.valueOf(cuentaD100));
+            binding.idTotalResultado.setText(String.valueOf(totalResultado));
+        }
 
         binding.idD4.setOnClickListener(new View.OnClickListener(){
             @Override
